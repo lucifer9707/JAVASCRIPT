4ed8b7b19c423bcc87b615ebc49a45001927b12f
Arun Rawat <86506750+lucifer9707@users.noreply.github.com>
2025-12-25 23:53:25 +0530

    Update array.js

--- array.js
+++ array.js
@@ -1,11 +1,11 @@
-// let fruits=["apple","kiwi","orange","banana"];
-// console.log(fruits[2]);
-// console.log(fruits.length);
+ let fruits=["apple","kiwi","orange","banana"];
+ console.log(fruits[2]);
+ console.log(fruits.length);
 
-// console.log("------------------");
+ console.log("------------------");
 
-// for(let i=0;i<fruits.length;i++){
-//     console.log(fruits[i]);
-// }
+ for(let i=0;i<fruits.length;i++){
+     console.log(fruits[i]);
+ }
 
 
