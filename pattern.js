07703241ce63aef2c6f9b599c8651b9a63c02fa7
Arun Rawat <86506750+lucifer9707@users.noreply.github.com>
2025-12-25 23:54:21 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -20,10 +20,10 @@
 // 
 // Inverted Pattern
 
-// for(let i=4;i>=1;i--){
-//     let str="";
-//     for(let j=1;j<=i;j++){
-//         str+="*";
-//     }
-//     console.log(str);
-// }
+ for(let i=4;i>=1;i--){
+     let str="";
+     for(let j=1;j<=i;j++){
+         str+="*";
+     }
+     console.log(str);
+ }
